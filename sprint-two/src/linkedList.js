b31616631c5ff4031c8fc78788c454c2ec43285b
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-02-25 10:51:55 -0800

    Reset the solo work so that only the pair information is displayed.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      var newNode = Node(value);
+      list.tail.next = newNode.value;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.tail;
+    return removed;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    } else {
+      return false;
+    }
+    // for (var property in list) {
+    //   console.log(list.tail);
+    //   for (var j in list[property]) {
+    //     // console.log(j);
+    //     return list[key][j] === target;
+    //   }
+    // }
   };
 
   return list;
