4155d41665fe12fbbe6a85e5fee7fbede1b8004f
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-02-24 16:12:48 -0800

    Completed stack.js and queue.js for the pseudoclassical instantiation.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.top = 0;
+  this.bottom = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.top] = value;
+  this.top++;
+};
+
+Queue.prototype.dequeue = function() {
+  var dequeued = this.storage[this.bottom];
+  delete this.storage[this.bottom];
+  this.bottom++;
+  return dequeued;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
