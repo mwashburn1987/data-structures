8ba87cd664c513f402b81439a3c84efe506264d0
Michael Washburn <m.washburn@mail.com>
2022-02-24 16:20:28 -0800

    completed pseudoclassical queue, starting es6 next

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+Queue.prototype.enqueue = function(value) {
+  let keys = Object.keys(this.storage);
+  let index = keys[keys.length - 1] + 1;
+  this.storage[index] = value;
+};
+Queue.prototype.dequeue = function(value) {
+  let keys = Object.keys(this.storage);
+  let index = keys[0];
+  let dequeued = this.storage[index];
+  delete this.storage[index];
+  return dequeued;
+};
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+var queue = new Queue;
 
 
