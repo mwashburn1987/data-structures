3a7b523945bceaa13ed8cf3f5a0cae3e21589a40
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-02-24 16:19:20 -0800

    Completed stack.js and queue.js for the es6 instantiation.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.top = 0;
   }
 
+  push(value) {
+    this.storage[this.top] = value;
+    this.top++;
+  }
+
+  pop() {
+    this.top--;
+    var popped = this.storage[this.top];
+    delete this.storage[this.top];
+    return popped;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
