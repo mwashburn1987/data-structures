e3d468a99733a1e42105b95c64cf4021a836731d
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-02-25 16:20:40 -0800

    Completed tree.js with pair.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,29 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  console.log(this.value);
+  if (this.value === target) {
+    return true;
+  }
+  for (let i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
