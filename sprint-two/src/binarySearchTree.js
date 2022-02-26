add28e4dc49e1da97089f511710e444fa260e8df
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-02-26 12:32:01 -0800

    Set up the structure for the binarySearchTree.js prior to lunch.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,24 @@
 var BinarySearchTree = function(value) {
+  var newBinaryTree = {};
+  newBinaryTree.value = value;
+  newBinaryTree.left = [];
+  newBinaryTree.right = [];
+  _.extend(newBinaryTree, binaryTreeMethods);
+  return newBinaryTree;
 };
 
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+};
+
+binaryTreeMethods.contains = function(value) {
+
+};
+
+binaryTreeMethods.depthFirstLog = function(callback) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
